77a468d920e5b524c842587b60d057f8b651aa2a
Spenson Hidalgo <alfigenser@gmail.com>
2019-12-12 20:35:46 -0700

    adding starting project

---
+++ build/precache-manifest.aad4734ad97128fe1cf0e07bd12ad707.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc972177e2bd9f288613080e6cae3bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2912d0eed528924762df",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64ef9f9ea8a08ebcd8eb",
+    "url": "/static/js/2.c588ade7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c588ade7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2912d0eed528924762df",
+    "url": "/static/js/main.1e79fe6b.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
